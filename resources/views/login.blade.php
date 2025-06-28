b01d75398a036c753fb90f33bd391f298be1e315
JBAUTISTA <bh202321940@alm.buap.mx>
2025-06-28 17:12:01 -0600

    cambio1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    
+    <h1></h1>Login Page</h1>
+    <form action="/login" method="POST">
+        @csrf
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <button type="submit">Login</button>
+    </form>
 </body>
 </html>
