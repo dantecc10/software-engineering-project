81764c6cb011120cb628aa9c5db42758059dcf4f
JBAUTISTA <bh202321940@alm.buap.mx>
2025-06-28 17:26:28 -0600

    renames1

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,10 @@ Route::get('/', function () {
 
 // Ruta para la vista de inicio (index)
 Route::get('/index', function () {
-    // Muestra la vista index.blade.php
     return view('index');
 });
 
 // Ruta para la vista de login personalizada
 Route::get('/login', function () {
-    // Muestra la vista login.blade.php
     return view('login');
 });
