74befe73dcda87f8fe1efffeb5287f6f7ce04ad1
JBAUTISTA <bh202321940@alm.buap.mx>
2025-06-28 16:50:30 -0600

    rutas para index/login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Ruta principal, muestra la vista de bienvenida
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Ruta para la vista de inicio (index)
+Route::get('/index', function () {
+    // Muestra la vista index.blade.php
+    return view('index');
+});
+
+// Ruta para la vista de login
+Route::get('/login', function () {
+    // Muestra la vista login.blade.php
+    return view('login');
 });
