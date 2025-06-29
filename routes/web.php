3bd62517b4639dd6c328f9a2564cd41db9ee4150
JBAUTISTA <bh202321940@alm.buap.mx>
2025-06-28 19:25:35 -0600

    nuevas rutas

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 // Ruta principal, muestra la vista de bienvenida
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 // Ruta para la vista de inicio (index)
@@ -20,4 +20,14 @@ Route::get('/login', function () {
 // Ruta para la vista de 404 personalizada
 Route::get('/404', function () {
     return view('404');
+});
+
+// Ruta para la vista de preguntas frecuentes
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+// Ruta para la vista de signup
+Route::get('/signup', function () {
+    return view('signup');
 });
