d5e413c75a3dd4c7ef510723d16d6a0dd3f66cac
JBAUTISTA <bh202321940@alm.buap.mx>
2025-06-30 00:53:14 -0600

    bladenuevo

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Cash Flow')</title>
+    <!-- Agrega aquí tus links de CSS globales -->
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
